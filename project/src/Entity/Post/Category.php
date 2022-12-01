3eb49b6c0de57c70e9d09a8f9d143a46532681ae
remy <moro.remy879@gmail.com>
2022-12-01 22:38:23 +0100

    "mise en place fixtures category  "

--- project/src/Entity/Post/Category.php
+++ project/src/Entity/Post/Category.php
@@ -2,10 +2,19 @@
 
 namespace App\Entity\Post;
 
-use App\Repository\Post\CategoryRepository;
+use Cocur\Slugify\Slugify;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\Post\CategoryRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+#[UniqueEntity(
+    'slug',
+    message: 'Ce slug existe déjà'
+)]
+
 class Category
 {
     #[ORM\Id]
@@ -14,17 +23,39 @@ class Category
     private ?int $id = null;
 
 
-    #[ORM\Column('string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    #[Assert\NotBlank()]
     private string $name;
 
 
-    #[ORM\Column('string', length: 255)]
-    private ?string $slug = '';
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    #[Assert\NotBlank()]
+    private string $slug = '';
 
-
+    #[ORM\Column(type: 'text', nullable: true)]
     private ?string $description = null;
 
-  
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Assert\NotNull()]
+    private \DateTimeImmutable $createdAt;
+
+
+
+
+
+    public function __construct()
+    {
+
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+
+
+    #[ORM\PrePersist]
+    function PrePersist()
+    {
+        $this->slug = (new Slugify())->slugify($this->name);
+    }
 
 
 
@@ -32,5 +63,69 @@ class Category
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+
+
+    public function setSlug(?string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+
+    function __toString()
+    {
+        return $this->name;
+    }
+
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
     }
 }
